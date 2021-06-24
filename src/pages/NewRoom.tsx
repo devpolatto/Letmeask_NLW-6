33f84130a1977dcbd724ca309dc65b44d3901f0f
devpolatto <polatto.xcode.job@gmail.com>
2021-06-24 14:08:45 -0300

    add record to FIrebase DB

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAuth } from '../hooks/useAuth';
@@ -8,10 +9,28 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss';
+import { database } from 'src/services/firebase';
 
 const NewRoom: React.FC = () => {
 
     const { user } = useAuth()
+    const [newRoom, setNewRoom] = useState('')
+
+    async function handleCreateNewRoom(event: FormEvent) { // create a new room
+        event.preventDefault(); // Prevent native html redirects the <farm/> event
+
+        // The trim() method removes white space from the beginning and/or end of a text.
+        if (newRoom.trim() === '') {
+            return
+        }
+
+        const roomRef = database.ref('rooms')
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        })
+    }
 
     return (
         <div id="page-auth">
@@ -25,8 +44,13 @@ const NewRoom: React.FC = () => {
                     <img src={logoImg} alt="Letmeask" />
                     <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" placeholder={'Nome da sala'} />
+                    <form onSubmit={handleCreateNewRoom}>
+                        <input
+                            type="text"
+                            placeholder={'Nome da sala'}
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
+                        />
                         <Button>
                             Criar sala
                         </Button>
