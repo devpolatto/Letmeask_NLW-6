674f861cc2bb70e0a771326f1d517fc4bc305d5a
devpolatto <polatto.xcode.job@gmail.com>
2021-06-24 20:48:57 -0300

    page Room finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
+import { useParams } from 'react-router-dom'
+
+import RoomCode from '../RoomCode';
+
 import './styles.scss';
 
 import logoImg from '../../assets/images/logo.svg';
 
+type RoomParams = {
+    id: string;
+}
+
 const Header: React.FC = () => {
+
+    // allows getting the parameters passed in the URL
+    const params = useParams<RoomParams>();
+
     return (
         <header>
             <div className="content">
                 <img src={logoImg} alt="Letmask" />
-                <div>codigo</div>
+                <RoomCode code={params.id} />
             </div>
         </header>
     );
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,31 @@
+import Header from '../components/Header';
+import Button from 'src/components/Button';
+
+import '../styles/room.scss';
+
 const Room: React.FC = () => {
     return (
-        <h2>Room</h2>
-    );
+        <div id="page-room">
+
+            <Header />
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+                <form>
+                    <textarea placeholder="O que voce quer perguntar?"></textarea>
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+
+            </main>
+
+        </div>
+    )
 }
 
 export default Room;
