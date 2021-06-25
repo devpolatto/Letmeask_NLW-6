baab65bce53bd744ec043a77f10e00f9f8da9d07
devpolatto <polatto.xcode.job@gmail.com>
2021-06-24 21:27:19 -0300

    Sending questions to the DB

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,54 @@
+import { useState, FormEvent } from 'react';
+import { useParams } from 'react-router-dom'
+
+import { useAuth } from 'src/hooks/useAuth';
+
 import Header from '../components/Header';
 import Button from 'src/components/Button';
 
+import { database } from 'src/services/firebase';
+
 import '../styles/room.scss';
 
+type RoomParams = {
+    id: string;
+}
+
 const Room: React.FC = () => {
+
+    // allows getting the parameters passed in the URL
+    const params = useParams<RoomParams>();
+    const [newQuestion, setNewQuestion] = useState('')
+    const { user } = useAuth();
+
+    const roomId = params.id;
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            throw new Error('You must be logged in')
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false,
+            isAnswered: false
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question)
+
+        setNewQuestion('')
+
+    }
+
     return (
         <div id="page-room">
 
@@ -14,11 +59,24 @@ const Room: React.FC = () => {
                     <h1>Sala React</h1>
                     <span>4 perguntas</span>
                 </div>
-                <form>
-                    <textarea placeholder="O que voce quer perguntar?"></textarea>
+                <form onSubmit={handleSendQuestion}>
+                    <textarea
+                        placeholder="O que voce quer perguntar?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
+                    ></textarea>
                     <div className="form-footer">
-                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        {
+                            user ? (
+                                <div className="user-info">
+                                    <img src={user.avatar} alt={user.name} />
+                                    <span>{user.name}</span>
+                                </div>
+                            ) : (
+                                <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                            )
+                        }
+                        <Button disabled={!user} type="submit">Enviar pergunta</Button>
                     </div>
                 </form>
 
