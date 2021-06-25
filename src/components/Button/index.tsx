31af7f1bb0ef7f9dcc8df5c0ccd18d69f00591d0
devpolatto <polatto.xcode.job@gmail.com>
2021-06-25 08:33:10 -0300

    add a new property in Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,13 @@ import { ButtonHTMLAttributes } from 'react'
 
 import './styles.scss'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean;
+}
 
-const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ isOutlined = false, ...props }: ButtonProps) => {
     return (
-        <button className="button" color="" {...props} />
+        <button className={`button ${isOutlined ? 'outlined' : ''}`} color="" {...props} />
     );
 }
 
